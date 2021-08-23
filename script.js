8a6957f2db183806fec49553733d34a1e811deb0
thekonly <joaopedro_m@icloud.com>
2021-08-22 23:53:51 -0400

    Personal Website Finalized

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ const h2 = document.createElement("h2");
 h2.textContent = "";
 
 document.querySelector("body").appendChild(h2);
+
+const title = document.createElement("title");
+title.textContent= "This was created using JavaScript"
+
+document.querySelector("body").appendChild(title);
+    
+}
